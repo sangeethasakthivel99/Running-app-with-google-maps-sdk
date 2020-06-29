fcab93fd87d25fcacb8aeed168bd84521e120690
Sangeetha <sangeetha.s@coviam.com>
2020-06-29 10:24:52 +0530

    ui changes

--- app/src/main/java/com/sangeetha/runningapp/view/RunFragment.kt
+++ app/src/main/java/com/sangeetha/runningapp/view/RunFragment.kt
@@ -1,14 +1,25 @@
 package com.sangeetha.runningapp.view
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.sangeetha.runningapp.R
 import com.sangeetha.runningapp.viewModels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
+import kotlinx.android.synthetic.main.fragment_run.*
 
 @AndroidEntryPoint
 class RunFragment: Fragment(R.layout.fragment_run) {
 
     private val viewModel: MainViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        }
+    }
 }
